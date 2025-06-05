90a413a865a7701bceb87e32f0a530fd4b712084
kravchenski <max5045955@gmail.com>
2025-06-05 20:56:14 +0200

    fix: update vite config and GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/test-repoi/',
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/scss/_variables";`,
-        includePaths: [path.resolve(__dirname, './src')]
+        additionalData: `@import "./src/scss/_variables.scss";`,
+        includePaths: [path.resolve(__dirname, './src/scss')]
       }
     }
   },
